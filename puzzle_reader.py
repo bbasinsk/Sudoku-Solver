14f6ac9d1934d1c2d2955c061db3746b328c28ba
Ben Basinski <benbasinski97@gmail.com>
2017-08-23 20:07:22 -0700

    Implemented splitting puzzle into digits

--- puzzle_reader.py
+++ puzzle_reader.py
@@ -1,11 +1,77 @@
 import cv2
+import numpy as np
 
 
-def get_one_puzzle(img):
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # cv2.imshow('warped', warped)
+    # cv2.waitKey(0)
+    # return the warped image
+    return warped
+
+
+def save_one_puzzle(img, index):
 
     # Reading the image
     image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image, (600, 600))
+    image = cv2.resize(image, (400, 600))
     blurred = cv2.GaussianBlur(image, (5, 5), 0)
 
     # Apply binary threshold
@@ -15,14 +81,11 @@ def get_one_puzzle(img):
     thresh, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     corners = _get_corners(contours)
-
-    cv2.line(image, corners[0], corners[1], (255, 255, 255, 0), thickness=4)
-    cv2.line(image, corners[1], corners[2], (255, 255, 255, 0), thickness=4)
-    cv2.line(image, corners[2], corners[3], (255, 255, 255, 0), thickness=4)
-    cv2.line(image, corners[3], corners[0], (255, 255, 255, 0), thickness=4)
-
-    cv2.imshow('threshold', image)
-    cv2.waitKey(0)
+    warped = four_point_transform(image, np.array(corners))
+    warped = warped[16:, :]
+    warped = cv2.resize(warped, (300, 300))
+    if not cv2.imwrite('puzzles_crop/' + str(index) + '.jpg', warped):
+        print(index)
 
 
 def _get_corners(contours):
@@ -64,6 +127,6 @@ def _get_corners(contours):
 
     return corners
 
-for i in range(0, 12):
-    get_one_puzzle('')
-get_one_puzzle('sudoku.jpg')
+for i in range(1, 61):
+    # print(i)
+    save_one_puzzle('orig_images/' + str(i) + '.jpg', i)
---
+++ sudoku_splitter.py
@@ -0,0 +1,34 @@
+import cv2
+
+
+def _answers(index):
+    path = "puzzles/" + str(index) + ".sud"
+    file = open(path, 'r')
+    digits = []
+    file = file.read()
+    file = file.replace("\n", "")
+    file = file.replace(" ", "")
+
+    for index in range(0, 81):
+        digit = file[index]
+        digits.append(int(digit))
+    return digits
+
+
+for puz_ind in range(1, 7):
+    image = cv2.imread('puzzles_crop/' + str(puz_ind) + '.jpg', cv2.IMREAD_GRAYSCALE)
+    digits = _answers(puz_ind)
+
+    crop_size = int((image.shape[0])/9)
+    crops = []
+    for col in range(9):
+        for row in range(9):
+            crop = image[int(col * crop_size): int(col * crop_size + crop_size),
+                         int(row * crop_size): int(row * crop_size + crop_size)]
+            crops.append(crop)
+    for d in range(0, 81):
+        dig_folder = str(digits[d])
+        puz_path = '/puz' + str(puz_ind)
+        cv2.imwrite('digits/' + dig_folder + puz_path + 'pos' + str(d + 1) + '.jpg', crops[d])
+        # print('saved puz: ' + str(puz_ind) + ' pos: ' + str(d))
+
